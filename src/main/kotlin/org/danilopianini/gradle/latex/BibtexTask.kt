1c174959e8f5922d1de4e2383b5868c0309b0ccb
Danilo Pianini <danilo.pianini@gmail.com>
2020-04-21 17:04:29 +0200

    Style improvement

--- src/main/kotlin/org/danilopianini/gradle/latex/BibtexTask.kt
+++ src/main/kotlin/org/danilopianini/gradle/latex/BibtexTask.kt
@@ -2,9 +2,9 @@ package org.danilopianini.gradle.latex
 
 import org.gradle.api.GradleException
 import org.gradle.api.file.FileCollection
-import org.gradle.api.tasks.*
-import org.gradle.kotlin.dsl.withGroovyBuilder
-import java.io.File
+import org.gradle.api.tasks.Console
+import org.gradle.api.tasks.InputFiles
+import org.gradle.api.tasks.TaskAction
 import java.nio.file.Files
 import javax.inject.Inject
 
@@ -12,20 +12,19 @@ open class BibtexTask @Inject constructor(artifact: LatexArtifact) : LatexTask(a
 
     @InputFiles
     override val inputFiles: FileCollection = project.files(
-        *(listOf(artifact.aux, artifact.tex, artifact.bib).filterNotNull().toTypedArray()))
+        *(listOfNotNull(artifact.aux, artifact.tex, artifact.bib).toTypedArray()))
 
     @Console
-    override fun getDescription() =  "Uses BibTex to compile ${artifact.aux} into ${artifact.name}.bbl"
+    override fun getDescription() = "Uses BibTex to compile ${artifact.aux} into ${artifact.name}.bbl"
 
     /**
      * Execute bibtex command for given latex artifact.
-     *
-     * @param artifact Any Latex artifact with the tex and bib properties set.
      */
     @TaskAction
     fun bibTex() {
         if (!artifact.aux.exists()) {
-            throw GradleException("${artifact.aux.absolutePath} does not exist, cannot invoke ${extension.bibTexCommand.get()}")
+            throw GradleException("${artifact.aux.absolutePath} does not exist," +
+                    " cannot invoke ${extension.bibTexCommand.get()}")
         }
         if (Files.lines(artifact.aux.toPath()).anyMatch { it.contains("""\citation""") }) {
             "${extension.bibTexCommand.get()} ${artifact.aux.name}".runScript()
@@ -33,5 +32,4 @@ open class BibtexTask @Inject constructor(artifact: LatexArtifact) : LatexTask(a
             Latex.LOG.warn("No citation in ${artifact.aux.absolutePath}, bibtex not invoked.")
         }
     }
-
 }
